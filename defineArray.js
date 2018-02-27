994dcfe2bfb9cce3f9e4bb666dc23c386f340b3f
Pamela Marcum <mucramap@yahoo.com>
2018-02-27 05:45:46 -0500

    Update defineArray.js

--- defineArray.js
+++ defineArray.js
@@ -1,4 +1,4 @@
 var githubArr = ['apples','oranges','pears','grapes']; // wow, this works!  loads up as if I had typed it directly into my userscript!
-function tryThis() {
+function letsTryThis() {
   return ['ford','chevy','subaru','honda','mazda']; // this function does NOT seem to work in my userscript.
 }
