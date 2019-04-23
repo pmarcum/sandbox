fa19660738aa8500c3281ba5a1c0c0cd6f02aa6f
Pamela Marcum <mucramap@yahoo.com>
2019-04-22 21:57:35 -0700

    Update makeObj.js

--- makeObj.js
+++ makeObj.js
@@ -1,3 +1,3 @@
 var makeObj = [];
-makeObj.push({"a":"A", "b":"B", "ans":function(t,i){return this.a + t.replace(new RegExp(i,'g'),'Z');}});
-makeObj.push({"a":"B", "b":"C", "ans":function(t,i){return t.replace(new RegExp(i,'g'),'Y') + this.b;}});
+makeObj.push({"a":"A", "b":"B", "ans":function(t,i){return this.a + t.replace(new RegExp(i,'g'),'Z');}, "outside":function(){return outside;}});
+makeObj.push({"a":"B", "b":"C", "ans":function(t,i){return t.replace(new RegExp(i,'g'),'Y') + this.b;}, "outside":function(){return inside;}});
