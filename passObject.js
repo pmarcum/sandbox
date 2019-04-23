fa28108ce7582de8d9c449377b84cdfb3273941c
Pamela Marcum <mucramap@yahoo.com>
2019-04-22 20:11:51 -0700

    Update passObject.js

--- passObject.js
+++ passObject.js
@@ -1,4 +1,4 @@
 var testObj = [];
-testObj.push({"param1":"A", "param2":"B"});
-testObj.push({"param1":"1", "param2":"2"});
-testObj.push({"param1":"z", "param2":"y"});
+testObj.push({"param1":"A", "param2":"B", "answer": function(){return this.param1}});
+testObj.push({"param1":"1", "param2":"2", "answer": function(){return this.param1}});
+testObj.push({"param1":"z", "param2":"y", "answer": function(){return this.param1}});
