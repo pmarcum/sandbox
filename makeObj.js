a84146fd36a734c801e1fd0787a11ba3a787d742
Pamela Marcum <mucramap@yahoo.com>
2019-04-22 21:12:38 -0700

    Update makeObj.js

--- makeObj.js
+++ makeObj.js
@@ -1,3 +1,3 @@
 var makeObj = [];
-makeObj.push({"a":"A", "b":"B", "ans":function(){return a.this;}});
-makeObj.push({"a":"B", "b":"C", "ans":function(){return b.this;}});
+makeObj.push({"a":"A", "b":"B", "ans":function(){return this.a;}});
+makeObj.push({"a":"B", "b":"C", "ans":function(){return this.b;}});
