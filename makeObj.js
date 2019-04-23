b210c14c3c82fc3fe83c717099c9b62be1dfa63a
Pamela Marcum <mucramap@yahoo.com>
2019-04-22 22:06:50 -0700

    Update makeObj.js

--- makeObj.js
+++ makeObj.js
@@ -1,3 +1,4 @@
 var makeObj = [];
-makeObj.push({"a":"A", "b":"B", "ans":function(t,i){return this.a + t.replace(new RegExp(i,'g'),'Z');}, "outside":function(){return outside;}});
-makeObj.push({"a":"B", "b":"C", "ans":function(t,i){return t.replace(new RegExp(i,'g'),'Y') + this.b;}, "outside":function(){return inside;}});
+var ascii = GM_getResourceText("ascii").split(/\n/);
+makeObj.push({"a":"A", "b":"B", "ans":function(t){return ascii.findIndex(z => z == t);}});
+makeObj.push({"a":"B", "b":"C", "ans":function(t){return ascii.findIndex(z => z != t);}});
